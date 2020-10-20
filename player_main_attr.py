3faddfe1027721d69f436dd8e308f725dbdce9fc
chris-ac-malone <chris.ac.malone@gmail.com>
2020-10-19 23:41:11 -0600

    Finished PlayerAttr getters and setters

--- player_main_attr.py
+++ player_main_attr.py
@@ -10,24 +10,40 @@ class PlayerAttr:
     manaCurent = 0
 
     ###Methods###
-    def getName()
-    def setName()
+    def getName():
+        return __name
+    def setName(newName):
+        __name = newName
 
-    def getLevel()
-    def setLevel()
+    def getLevel():
+        return __level
+    def setLevel(newLevel):
+        __level = newLevel
 
-    def getExpCurrent()
-    def setExpCurrent()
-    def getExpNeed()
-    def setExpNeed()
+    def getExpCurrent():
+        return __expCurrent
+    def setExpCurrent(newExpCurrent):
+        __expCurrent = newExpCurrent
+    def getExpNeed():
+        return __expNeed
+    def setExpNeed(newExpNeed):
+        __expNeed = newExpNeed
 
-    def getHealthCurrent()
-    def setHealthCurrent()
-    def getHealthMax()
-    def setHealthMax()
+    def getHealthCurrent():
+        return healthCurrent
+    def setHealthCurrent(newHealthCurrent):
+        healthCurrent = newHealthCurrent
+    def getHealthMax():
+        return __healthMax
+    def setHealthMax(newHealthMax):
+        __healthMax = newHealthMax
 
-    def getManaCurrent()
-    def setManaCurrent()
-    def getManaMax()
-    def setManaMax()
+    def getManaCurrent():
+        return manaCurent
+    def setManaCurrent(newManaCurrent):
+        manaCurrent = newManaCurrent
+    def getManaMax():
+        return __manaMax
+    def setManaMax(newManaMax):
+        __manaMax = newManaMax
 
