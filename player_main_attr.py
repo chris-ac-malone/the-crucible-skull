204b12d35168114250cb557e6655e014505e5cd4
Stirling Robison <stirlingrobison@Stirlings-MacBook-Air.local>
2020-10-19 19:53:59 -0600

    Added player main stats

---
+++ player_main_attr.py
@@ -0,0 +1,34 @@
+class Player:
+    ###Member Variables###
+    __name = ""
+    __level = 0
+    __expCurrent = 0
+    __expNeed = 0
+    __healthMax = 0
+    healthCurrent = 0
+    __manaMax = 0
+    manaCurent = 0
+
+    ###Methods###
+    def getName()
+    def setName()
+
+    def getLevel()
+    def setLevel()
+
+    def getExpCurrent()
+    def setExpCurrent()
+    def getExpNeed()
+    def setExpNeed()
+
+    def getHealthCurrent()
+    def setHealthCurrent()
+    def getHealthMax()
+    def setHealthMax()
+
+    def getManaCurrent()
+    def setManaCurrent()
+    def getManaMax()
+    def setManaMax()
+
+    
---
+++ player_main_stats.py
@@ -0,0 +1,3 @@
+###Player Stats###
+
+###Member Variables###
