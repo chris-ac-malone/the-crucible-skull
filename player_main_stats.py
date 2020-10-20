ea24310cc1d0c03c7cd4f62f48f1d155d14e6385
chris-ac-malone <chris.ac.malone@gmail.com>
2020-10-20 12:58:22 -0600

    Added playerstats getters and setters

--- player_main_stats.py
+++ player_main_stats.py
@@ -10,17 +10,27 @@ class PlayerStats:
     __speed = 0
 
     ###Member Methods###
-    def getStrength()
-    def setStrength()
+    def getStrength(self):
+        return self.__strength
+    def setStrength(self, newStrength):
+        self.__strength = newStrength
 
-    def getEndurance()
-    def setEndurance()
+    def getEndurance(self):
+        return self.__endurance
+    def setEndurance(self, newEndurance):
+        self.__endurance = newEndurance
 
-    def getIntelligence()
-    def setIntelligence()
+    def getIntelligence(self):
+        return self.__intelligence
+    def setIntelligence(self, newIntelligence):
+        self.__intelligence = newIntelligence
 
-    def getCharisma()
-    def setCharisma()
+    def getCharisma(self):
+        return self.__charisma
+    def setCharisma(self, newCharisma):
+        self.__charisma = newCharisma
 
-    def getSpeed()
-    def setSpeed()
+    def getSpeed(self):
+        return self.__speed
+    def setSpeed(self, newSpeed):
+        self.__speed = newSpeed
