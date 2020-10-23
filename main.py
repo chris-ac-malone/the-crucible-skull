5492fe8c1a71a2c310b64051866c5406508dfabd
chris-ac-malone <chris.ac.malone@gmail.com>
2020-10-23 00:42:19 -0600

    added a clear to the beginning of the program before the loop, it's for program sexyness

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import game_loop
 import player_main_attr as player
 import player_main_stats as stats
-import additional_functions
+import additional_functions as extra
 import json
 
 ###This will grab the current room from the save file, possibly the party's variable### 
@@ -9,5 +9,8 @@ with open('save_files/defaultSave.json') as saveDataJson:
     saveData = json.load(saveDataJson)
     saveDataJson.close()
 currentRoom = saveData["saves"]["currentRoom"]
+
+extra.clear()
+
 run = game_loop.GameLoop(currentRoom)
 run.loop()
